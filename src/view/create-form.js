c5d885b396d50536ead13a83ecc6fdb36938f5e0
Godmode_ON <github1git@gmail.com>
2025-05-20 23:34:36 +0500

    22

--- src/view/create-form.js
+++ src/view/create-form.js
@@ -1,6 +1,6 @@
 const createFormTemplate = () => (
   `<form class="trip-events__item  event  event--edit">
-    <!-- Здесь твоя форма создания новой точки маршрута -->
+    <!--  -->
     <header class="event__header">
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
@@ -8,7 +8,7 @@ const createFormTemplate = () => (
           <img class="event__type-icon" width="17" height="17" src="img/icons/taxi.png" alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
-        <!-- И т.д., больше полей формы по твоей разметке -->
+        <!--  -->
       </div>
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
@@ -21,10 +21,10 @@ const createFormTemplate = () => (
           <option value="Geneva"></option>
         </datalist>
       </div>
-      <!-- Здесь ещё поля формы, даты, стоимость, и другое по твоему макету -->
+      <!--  -->
     </header>
     <section class="event__details">
-      <!-- Отдельные секции для опций, описания поездки, и т.д. -->
+      <!--  -->
     </section>
   </form>`
 );
