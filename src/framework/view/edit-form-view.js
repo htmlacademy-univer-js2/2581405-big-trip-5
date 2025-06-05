d9696efd51ad366f654a7def07413e3f9eeed4d4
Godmode_ON <150623220+ConsoleSystemPrint@users.noreply.github.com>
2025-06-05 19:17:54 +0500

    8

--- src/framework/view/edit-form-view.js
+++ src/framework/view/edit-form-view.js
@@ -1,4 +1,4 @@
-import AbstractStatefulView from './framework/view/abstract-stateful-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
