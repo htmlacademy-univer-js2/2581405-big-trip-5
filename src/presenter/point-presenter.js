7ad008c9d59ea9dcc5ebe184af67b31f9cc5a53c
Godmode_ON <150623220+ConsoleSystemPrint@users.noreply.github.com>
2025-06-05 19:14:41 +0500

    7

--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import PointView from '../view-v/point.js';
-import EditFormView from '../view/edit-form-view.js';
+import EditFormView from '../framework/view/edit-form-view.js';
 import { render, replace, remove } from '../framework/render.js';
 
 export default class PointPresenter {
